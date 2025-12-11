9a79436f1449d9ba3de9abd704136599e085d897
TanmoyWG01 <tanmoy.webgen@gmail.com>
2025-12-11 16:42:42 +0530

    Initial commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Post } from '@nestjs/common';
+
+@Controller('auth') //auth/register
+export class AuthController {
+
+    @Post('register')
+    register(){
+        return { message: 'User registered successfully' };
+    }
+}
