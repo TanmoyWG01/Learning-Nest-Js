75ff7212a48506d35c55c44bbbefeef869e9763e
TanmoyWG01 <tanmoy.webgen@gmail.com>
2025-12-11 18:44:01 +0530

    refactor AuthService to use UserService and accept RegisterUserDto in registerUser method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { RegisterUserDto } from './dto/registerUser.dto';
 
 @Injectable()
-export class AuthService {
-    registerUser() {
-        return { message: 'User registered successfully from AuthServices' };
+export class AuthService { 
+    constructor(private readonly userService: UserService   ) {}
+    registerUser(registerUserDTO: RegisterUserDto) {
+        console.log('Registering user with data:', registerUserDTO);
+        const userCreationResult = this.userService.createUser();
+        return userCreationResult;
     }
 }
