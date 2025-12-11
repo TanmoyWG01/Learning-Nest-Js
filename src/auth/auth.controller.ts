c391921979a18c02c67153810122db89fab4bd72
TanmoyWG01 <tanmoy.webgen@gmail.com>
2025-12-11 17:16:20 +0530

    implement AuthService injection in AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,15 @@
 import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth') //auth/register
 export class AuthController {
+    authService: AuthService;
+    constructor(authService: AuthService) {
+        this.authService = authService;
+    }
     @Post('register')
     register(){
+        this.authService.registerUser();
         return { message: 'User registered successfully' };
     }
 }
