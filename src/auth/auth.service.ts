03d792d4544b1a34ba723ed936f892b4618db10b
TanmoyWG01 <tanmoy.webgen@gmail.com>
2025-12-15 19:56:09 +0530

    add mongo service configuration and bcrypt type definitions; update AuthService to hash passwords and create users

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { RegisterUserDto } from './dto/registerUser.dto';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService { 
     constructor(private readonly userService: UserService   ) {}
-    registerUser(registerUserDTO: RegisterUserDto) {
+    async registerUser(registerUserDTO: RegisterUserDto) {
         console.log('Registering user with data:', registerUserDTO);
-        const userCreationResult = this.userService.createUser();
+
+        const saltRounds = 10;
+        const hash = await bcrypt.hash(registerUserDTO.password, saltRounds);
+        console.log('Hashed password:', hash);
+
+
+        const userCreationResult = this.userService.createUser({...registerUserDTO, password: hash });
         return userCreationResult;
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { RegisterUserDto } from 'src/auth/dto/registerUser.dto';
 
 @Injectable()
 export class UserService {
-    createUser() {
+    createUser(registerUserDTO: RegisterUserDto) {
         return { message: 'User created successfully from UserService' };
     }
 }
