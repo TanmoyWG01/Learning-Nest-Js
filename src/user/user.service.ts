0403346a4f30db86ecd1fcafc78da4f567bcef55
TanmoyWG01 <tanmoy.webgen@gmail.com>
2026-07-30 17:01:47 +0530

    feat: implement user creation using Mongoose model in UserService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { RegisterUserDto } from '../auth/dto/registerUser.dto';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class UserService {
-  createUser(registerUserDTO: RegisterUserDto) {
-    return { message: 'User created successfully from UserService' };
+
+  constructor(@InjectModel(User.name) private UserModel: Model<User>) {}
+
+  async createUser(registerUserDTO: RegisterUserDto) {
+
+    return await this.UserModel.create({
+      fname: registerUserDTO.fname,
+      lname: registerUserDTO.lname,
+      email: registerUserDTO.email,
+      password: registerUserDTO.password,
+    })
+
+    // return { message: 'User created successfully from UserService' };
   }
 }
